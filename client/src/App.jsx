baed3eb0d9e5f9453a37e841ebefb40bef670cb2
ahteshamulhasan <ahteshamulhasan2@gmail.com>
2025-08-26 23:01:42 +0600

    server add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home'
+import Cart from './Pages/Cart'
+import Navbar from './Components/Navbar'
+import NotFound from './Pages/NotFound'
 export default function App() {
   return (
-    <div className='text-teal-500'>App</div>
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/cart/:id' element={<Cart />} />
+          <Route path='*' element={<NotFound/>} />
+      </Routes>
+      </BrowserRouter>
+    </>
   )
 }
